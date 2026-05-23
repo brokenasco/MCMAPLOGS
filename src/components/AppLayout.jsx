989faeb51ed68849fef387048c3bb45fb811aa89
EZLogs4you <keatonray99@gmail.com>
2026-05-22 23:04:59 -0400

    Remove global subscription banner

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header.jsx';
-import SubscriptionBanner from './SubscriptionBanner.jsx';
 
 export default function AppLayout() {
   return (
     <div className="min-h-screen bg-field text-ink">
       <Header />
-      <SubscriptionBanner />
       <main>
         <Outlet />
       </main>
